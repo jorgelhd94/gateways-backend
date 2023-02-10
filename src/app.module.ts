ffe0f3e5c0225a3a6f15b6ee87d399c169067289
Jorge Luis Hernandez Dueñas <jorgelhd94@gmail.com>
2023-02-10 17:07:45 -0500

    fix schemas

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,16 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { GatewaysModule } from './gateways/gateways.module';
 import { DevicesModule } from './devices/devices.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [GatewaysModule, DevicesModule],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://mongo:mongo@cluster0.3qsyk6t.mongodb.net/gatewaysdb',
+    ),
+    GatewaysModule,
+    DevicesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/gateways/schemas/gateway.schema.ts
+++ src/gateways/schemas/gateway.schema.ts
@@ -5,7 +5,12 @@ import { IGateway } from '../interfaces/gateway.interface';
 const gatewaySchema = new Schema({
   serialNumber: { type: String, required: true, unique: true },
   name: { type: String, required: true },
-  ipAddress: { type: String, required: true },
+  ipAddress: {
+    type: String,
+    required: true,
+    validate:
+      /\b(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\b/,
+  },
   devices: [DeviceModel.schema],
 });
 
