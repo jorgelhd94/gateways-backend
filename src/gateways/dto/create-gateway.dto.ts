54b6e19b01b989e1fe5db1985e6ce599be873859
Jorge Luis Hernandez Dueñas <jorgelhd94@gmail.com>
2023-02-11 09:17:20 -0500

    add validations

--- src/gateways/dto/create-gateway.dto.ts
+++ src/gateways/dto/create-gateway.dto.ts
@@ -1,1 +1,16 @@
-export class CreateGatewayDto {}
+import { IsString, IsIP, IsOptional } from 'class-validator';
+import { Device } from 'src/devices/schemas/device.schema';
+
+export class CreateGatewayDto {
+  @IsString()
+  serialNumber: string;
+
+  @IsString()
+  name: string;
+
+  @IsIP(4)
+  ipAddress: string;
+
+  @IsOptional()
+  devices: Device[];
+}
