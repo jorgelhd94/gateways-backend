50e3e5ba1d5ae047643bf7f63049071dfa59b5d4
Jorge Luis Hernandez Dueñas <jorgelhd94@gmail.com>
2023-02-12 17:11:46 -0500

    Remove password requirement

--- src/auth/dto/create-user.dto.ts
+++ src/auth/dto/create-user.dto.ts
@@ -12,11 +12,7 @@ export class CreateUserDto {
 
   @IsString()
   @MinLength(6)
-  @MaxLength(50)
-  @Matches(/(?:(?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
-    message:
-      'The password must have a Uppercase, lowercase letter and a number',
-  })
+  @MaxLength(10)
   password: string;
 
   @IsString()
