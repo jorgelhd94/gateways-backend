8fd470070378f35ce83d90a604f67881771dbf51
Jorge Luis Hernandez Dueñas <jorgelhd94@gmail.com>
2023-02-12 17:59:25 -0500

    add cors and fix password

--- src/auth/dto/login-user.dto.ts
+++ src/auth/dto/login-user.dto.ts
@@ -1,10 +1,4 @@
-import {
-  IsEmail,
-  IsString,
-  Matches,
-  MaxLength,
-  MinLength,
-} from 'class-validator';
+import { IsEmail, IsString, MaxLength, MinLength } from 'class-validator';
 
 export class LoginUserDto {
   @IsEmail()
@@ -12,10 +6,6 @@ export class LoginUserDto {
 
   @IsString()
   @MinLength(6)
-  @MaxLength(50)
-  @Matches(/(?:(?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
-    message:
-      'The password must have a Uppercase, lowercase letter and a number',
-  })
+  @MaxLength(10)
   password: string;
 }
--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger/dist';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   app.setGlobalPrefix('api/v1');
   app.useGlobalPipes(
     new ValidationPipe({
