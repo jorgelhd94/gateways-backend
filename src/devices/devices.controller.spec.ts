45a06debeeccc57fade38314537758790cf1add2
Jorge Luis Hernandez Dueñas <jorgelhd94@gmail.com>
2023-02-16 17:42:16 -0500

    add controller tests

--- src/devices/devices.controller.spec.ts
+++ src/devices/devices.controller.spec.ts
@@ -1,9 +1,36 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { DevicesController } from './devices.controller';
 import { DevicesService } from './devices.service';
+import { CreateDeviceDto } from './dto/create-device.dto';
+import { IDevice } from './interfaces/device.interface';
 
 describe('DevicesController', () => {
   let controller: DevicesController;
+  let service: DevicesService;
+
+  const mockGateway = {
+    serialNumber: '123',
+    name: 'asd',
+    ipAddress: '10.0.1.1',
+    _id: 'a id',
+    devices: [],
+  };
+
+  const createDeviceDto: CreateDeviceDto = {
+    uid: 123,
+    vendor: 'asd',
+    dateCreated: new Date('2023-12-12'),
+    status: 'online',
+  };
+
+  const mockDevice: IDevice = {
+    id: 'other id',
+    uid: 123,
+    vendor: 'asd',
+    dateCreated: new Date('2023-12-12'),
+    status: 'online',
+    gatewayId: mockGateway,
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -11,15 +38,61 @@ describe('DevicesController', () => {
       providers: [
         {
           provide: DevicesService,
-          useValue: {},
+          useValue: {
+            create: jest.fn().mockResolvedValue(createDeviceDto),
+          },
         },
       ],
     }).compile();
 
     controller = module.get<DevicesController>(DevicesController);
+    service = module.get<DevicesService>(DevicesService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
   });
+
+  describe('create()', () => {
+    it('should create a new device', async () => {
+      const createSpy = jest
+        .spyOn(service, 'create')
+        .mockResolvedValueOnce(mockDevice);
+
+      await controller.create('a id', createDeviceDto);
+      expect(createSpy).toHaveBeenCalledWith('a id', createDeviceDto);
+    });
+  });
+
+  // describe('findAll()', () => {
+  //   it('should find all gateways by user', async () => {
+  //     expect(controller.findAll(mockUser)).resolves.toEqual(mockListGateways);
+  //     expect(service.findAll).toHaveBeenCalled();
+  //   });
+  // });
+
+  // describe('findOne()', () => {
+  //   it('should find a gateway by id', async () => {
+  //     expect(controller.findOne('a id')).resolves.toEqual(mockGateway);
+  //     expect(service.findOne).toHaveBeenCalled();
+  //   });
+  // });
+
+  // describe('update()', () => {
+  //   it('should update a gateway by id', async () => {
+  //     const updateSpy = jest
+  //       .spyOn(service, 'update')
+  //       .mockResolvedValueOnce(mockUpdatedGateway);
+
+  //     await controller.update('a id', updateGatewayDto);
+  //     expect(updateSpy).toHaveBeenCalledWith('a id', updateGatewayDto);
+  //   });
+  // });
+
+  // describe('remove()', () => {
+  //   it('should remove a gateway by id', async () => {
+  //     expect(controller.remove('a id')).resolves.toEqual(mockGateway);
+  //     expect(service.remove).toHaveBeenCalled();
+  //   });
+  // });
 });
--- src/gateways/gateways.controller.spec.ts
+++ src/gateways/gateways.controller.spec.ts
@@ -1,14 +1,12 @@
 import { CreateGatewayDto } from './dto/create-gateway.dto';
-import { Device } from './../devices/schemas/device.schema';
 import { Test, TestingModule } from '@nestjs/testing';
 import { GatewaysController } from './gateways.controller';
 import { GatewaysService } from './gateways.service';
-import { Model } from 'mongoose';
-import { Gateway } from './schemas/gateway.schema';
-import { getModelToken } from '@nestjs/mongoose';
+import { UpdateGatewayDto } from './dto/update-gateway.dto';
 
 describe('GatewaysController', () => {
   let controller: GatewaysController;
+  let service: GatewaysService;
 
   const createGatewayDto: CreateGatewayDto = {
     serialNumber: '123',
@@ -16,11 +14,48 @@ describe('GatewaysController', () => {
     ipAddress: '10.0.1.1',
   };
 
+  const updateGatewayDto: UpdateGatewayDto = {
+    serialNumber: '12',
+    ipAddress: '192.0.1.1',
+  };
+
   const mockGateway = {
     serialNumber: '123',
     name: 'asd',
     ipAddress: '10.0.1.1',
     _id: 'a id',
+    devices: [],
+  };
+
+  const mockUpdatedGateway = {
+    serialNumber: '12',
+    name: 'asd',
+    ipAddress: '192.0.1.1',
+    _id: 'a id',
+    devices: [],
+  };
+
+  const mockListGateways = [
+    {
+      serialNumber: '123',
+      name: 'asd',
+      ipAddress: '10.0.1.1',
+      _id: 'a id',
+      devices: [],
+    },
+    {
+      serialNumber: '123',
+      name: 'asd',
+      ipAddress: '10.0.1.1',
+      _id: 'a id',
+      devices: [],
+    },
+  ];
+
+  const mockUser = {
+    email: 'lolo@email.cu',
+    fullname: 'Lolo',
+    password: 'lolo123',
   };
 
   beforeEach(async () => {
@@ -31,15 +66,63 @@ describe('GatewaysController', () => {
           provide: GatewaysService,
           useValue: {
             create: jest.fn().mockResolvedValue(createGatewayDto),
+            findOne: jest.fn().mockResolvedValue(mockGateway),
+            findAll: jest.fn().mockResolvedValue(mockListGateways),
+            update: jest.fn().mockResolvedValue(updateGatewayDto),
+            remove: jest.fn().mockResolvedValue(mockGateway),
           },
         },
       ],
     }).compile();
 
     controller = module.get<GatewaysController>(GatewaysController);
+    service = module.get<GatewaysService>(GatewaysService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('create()', () => {
+    it('should create a new gateway', async () => {
+      const createSpy = jest
+        .spyOn(service, 'create')
+        .mockResolvedValueOnce(mockGateway);
+
+      await controller.create(createGatewayDto, mockUser);
+      expect(createSpy).toHaveBeenCalledWith(createGatewayDto, mockUser);
+    });
+  });
+
+  describe('findAll()', () => {
+    it('should find all gateways by user', async () => {
+      expect(controller.findAll(mockUser)).resolves.toEqual(mockListGateways);
+      expect(service.findAll).toHaveBeenCalled();
+    });
+  });
+
+  describe('findOne()', () => {
+    it('should find a gateway by id', async () => {
+      expect(controller.findOne('a id')).resolves.toEqual(mockGateway);
+      expect(service.findOne).toHaveBeenCalled();
+    });
+  });
+
+  describe('update()', () => {
+    it('should update a gateway by id', async () => {
+      const updateSpy = jest
+        .spyOn(service, 'update')
+        .mockResolvedValueOnce(mockUpdatedGateway);
+
+      await controller.update('a id', updateGatewayDto);
+      expect(updateSpy).toHaveBeenCalledWith('a id', updateGatewayDto);
+    });
+  });
+
+  describe('remove()', () => {
+    it('should remove a gateway by id', async () => {
+      expect(controller.remove('a id')).resolves.toEqual(mockGateway);
+      expect(service.remove).toHaveBeenCalled();
+    });
   });
 });
