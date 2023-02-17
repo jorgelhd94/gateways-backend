a6bd040ec078d695501fb61c4e47abaebd04b9c1
Jorge Luis Hernandez Dueñas <jorgelhd94@gmail.com>
2023-02-17 09:08:59 -0500

    finish service tests

--- src/devices/devices.service.spec.ts
+++ src/devices/devices.service.spec.ts
@@ -1,23 +1,106 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { DevicesService } from './devices.service';
+import { Device } from './schemas/device.schema';
+import { Model } from 'mongoose';
+import { getModelToken } from '@nestjs/mongoose';
+import { CreateDeviceDto } from './dto/create-device.dto';
+import { IDevice } from './interfaces/device.interface';
+
+const mockUser = {
+  email: 'lolo@email.cu',
+  fullname: 'Lolo',
+  password: 'lolo123',
+};
+
+const mockGateway = {
+  serialNumber: '123',
+  name: 'asd',
+  ipAddress: '10.0.1.1',
+  _id: 'a id',
+  devices: [],
+};
+
+const createDeviceDto: CreateDeviceDto = {
+  uid: 123,
+  vendor: 'asd',
+  dateCreated: new Date('2023-12-12'),
+  status: 'online',
+};
+
+const mockDevice = {
+  uid: 123,
+  vendor: 'asd',
+  dateCreated: new Date('2023-12-12'),
+  status: 'online',
+};
+
+const mockListDevices: IDevice[] = [
+  {
+    id: 'first id',
+    uid: 123,
+    vendor: 'asd',
+    dateCreated: new Date('2023-12-12'),
+    status: 'online',
+    gatewayId: mockGateway,
+  },
+  {
+    id: 'second id',
+    uid: 456,
+    vendor: 'dsa',
+    dateCreated: new Date('2023-12-12'),
+    status: 'online',
+    gatewayId: mockGateway,
+  },
+];
 
 describe('DevicesService', () => {
   let service: DevicesService;
+  let model: Model<Device>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [
         {
           provide: DevicesService,
-          useValue: {},
+          useValue: {
+            create: jest.fn().mockResolvedValue(createDeviceDto),
+            findAll: jest.fn().mockResolvedValue(mockListDevices),
+          },
+        },
+        {
+          provide: getModelToken('Device'),
+          useValue: {
+            new: jest.fn().mockResolvedValue(mockDevice),
+            constructor: jest.fn().mockResolvedValue(mockDevice),
+            find: jest.fn(),
+            create: jest.fn(),
+            exec: jest.fn(),
+          },
         },
       ],
     }).compile();
 
     service = module.get<DevicesService>(DevicesService);
+    model = module.get<Model<Device>>(getModelToken('Device'));
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should create a new device', async () => {
+    jest
+      .spyOn(model, 'create')
+      .mockImplementationOnce(() => Promise.resolve(mockDevice));
+    const newGateway = await service.create('a id', createDeviceDto);
+    expect(newGateway).toEqual(mockDevice);
+  });
+
+  it('should return all devices', async () => {
+    jest.spyOn(model, 'find').mockReturnValue({
+      exec: jest.fn().mockResolvedValueOnce(mockListDevices),
+    } as any);
+    const devices = await service.findAll(mockUser);
+    expect(devices).toEqual(mockListDevices);
   });
 });
--- src/gateways/gateways.service.spec.ts
+++ src/gateways/gateways.service.spec.ts
@@ -1,23 +1,93 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { GatewaysService } from './gateways.service';
+import { getModelToken } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Gateway } from './schemas/gateway.schema';
+import { CreateGatewayDto } from './dto/create-gateway.dto';
+
+const mockUser = {
+  email: 'lolo@email.cu',
+  fullname: 'Lolo',
+  password: 'lolo123',
+};
+
+const mockGateway = {
+  serialNumber: '123',
+  name: 'asd',
+  ipAddress: '10.0.1.1',
+};
+
+const mockListGateways = [
+  {
+    serialNumber: '123',
+    name: 'asd',
+    ipAddress: '10.0.1.1',
+    _id: 'a id',
+    devices: [],
+  },
+  {
+    serialNumber: '123',
+    name: 'asd',
+    ipAddress: '10.0.1.1',
+    _id: 'a id',
+    devices: [],
+  },
+];
+
+const createGatewayDto: CreateGatewayDto = {
+  serialNumber: '123',
+  name: 'asd',
+  ipAddress: '10.0.1.1',
+};
 
 describe('GatewaysService', () => {
   let service: GatewaysService;
+  let model: Model<Gateway>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [
         {
           provide: GatewaysService,
-          useValue: {},
+          useValue: {
+            create: jest.fn().mockResolvedValue(createGatewayDto),
+            findAll: jest.fn().mockResolvedValue(mockListGateways),
+          },
+        },
+        {
+          provide: getModelToken('Gateway'),
+          useValue: {
+            new: jest.fn().mockResolvedValue(mockGateway),
+            constructor: jest.fn().mockResolvedValue(mockGateway),
+            find: jest.fn(),
+            create: jest.fn(),
+            exec: jest.fn(),
+          },
         },
       ],
     }).compile();
 
     service = module.get<GatewaysService>(GatewaysService);
+    model = module.get<Model<Gateway>>(getModelToken('Gateway'));
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should create a new gateway', async () => {
+    jest
+      .spyOn(model, 'create')
+      .mockImplementationOnce(() => Promise.resolve(mockGateway));
+    const newGateway = await service.create(createGatewayDto, mockUser);
+    expect(newGateway).toEqual(mockGateway);
+  });
+
+  it('should return all gateways', async () => {
+    jest.spyOn(model, 'find').mockReturnValue({
+      exec: jest.fn().mockResolvedValueOnce(mockListGateways),
+    } as any);
+    const gateways = await service.findAll(mockUser);
+    expect(gateways).toEqual(mockListGateways);
   });
 });
